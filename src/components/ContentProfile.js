2696a2cc6bbae59469b8acd10a340e86e20d5154
antony <antony.jacques06@gmail.com>
2021-10-06 22:33:46 +0200

    fix bug filter

--- src/components/ContentProfile.js
+++ src/components/ContentProfile.js
@@ -5,40 +5,31 @@ import json_data from '../data/site.json'
 import '../css/contentprofile.scss';
 
 const ContentProfile = ({photographerID}) => {
-    const [medias, setMedias] = useState([])
-    //const [ filtre, setFiltre ] = useState('popular')
+    const medias = []
+    const [ filtre, setFiltre] = useState('popular')
+    const [ order, setOrder ] = useState('DESC')
+    const designOrder = { "ASC" : "↑", "DESC" : "↓"}
 
     useEffect(() => {    
-        console.log(medias)
+        //console.log(medias)
     });
 
+    const filtre_existant = [{"name":"Popularité","function":"popular"},{"name":"Date","function":"date"},{"name":"Titre","function":"title"}]
 
     const onFiltreChange = (event) => {
-        filtre_existant[event.target.value].function.call()
-    }
-    const popular = (reverse = 'ASC') => {
-        reverse = reverse == 'ASC' ? 1 : -1;
-        setMedias((prevMedias) => [...prevMedias].sort((a, b) => {
-            return reverse * ((a.likes > b.likes) - (b.likes > a.likes)) 
-        }));
-        setMedias((prevMedias) => [...prevMedias].filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i));
-    }
-    const date = (reverse = 'ASC') => {
-        reverse = reverse == 'ASC' ? 1 : -1;
-        setMedias((prevMedias) => [...prevMedias].sort((a,b) => {
-            return reverse * ((new Date(a.date) > new Date(b.date)) - (new Date(b.date) > new Date(a.date)));
-        }));
-        setMedias((prevMedias) => [...prevMedias].filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i));
-    }
-    const title = (reverse = 'ASC') => {
-        reverse = reverse == 'ASC' ? 1 : -1;
-        setMedias((prevMedias) => [...prevMedias].sort((a, b) => {
-            return reverse * ((a.title > b.title) - (b.title > a.title)) 
-        }));
-        setMedias((prevMedias) => [...prevMedias].filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i));
-    }
-    const filtre_existant = [{"name":"Popularité","function":popular},{"name":"Date","function":date},{"name":"Titre","function":title}]
+        var inverse_order = "DESC"
+        order === "DESC" ? inverse_order = "ASC" : null
+        order === "ASC" ? inverse_order = "DESC" : null
+        filtre === filtre_existant[event.target.value].function ? setOrder(inverse_order) : null
 
+        setFiltre(filtre_existant[event.target.value].function)  
+    }
+
+   /* const onFiltreBlur = (event) => {
+        console.log("test",event)
+    }*/
+   
+    
     // function to retrieve all product
     const initialize_all_product = () => {
         json_data.media.map(
@@ -53,9 +44,37 @@ const ContentProfile = ({photographerID}) => {
     initialize_all_product()
 
 
-    const render_image = () => {
-        return medias.map(
-            (media,i) => { return <div key={i} className="product__content__item">
+    const render_image = (filter_select) => {
+
+        var array_render = medias;
+        var reverse = order == 'ASC' ? 1 : -1;
+
+        switch ( filter_select )
+        {
+            case 'popular':
+                    array_render = array_render.sort((a, b) => {
+                        return reverse * ((a.likes > b.likes) - (b.likes > a.likes)) 
+                    })
+                break;
+
+            case 'date':
+                    array_render = array_render.sort((a,b) => {
+                        return reverse * ((new Date(a.date) > new Date(b.date)) - (new Date(b.date) > new Date(a.date)));
+                    });
+                break;
+
+            case 'title':
+                    array_render = array_render.sort((a, b) => {
+                        return reverse * ((a.title > b.title) - (b.title > a.title)) 
+                    });
+                break;
+        }
+
+
+        return array_render.map(
+            (media,i) => {
+                console.log(media.likes,media.date,media.title)
+                return <div key={i} className="product__content__item">
                     {(media.image ? 
                     <img src={'../img/' + photographerID + '/' + media.image}></img> : 
                     <video poster={'../img/' + photographerID + '/' + media.video}>
@@ -75,17 +94,17 @@ const ContentProfile = ({photographerID}) => {
         <section className="product">
             <div className="product__filter">
                 <p>Trier par</p>
-                <select onChange={onFiltreChange}>
+                <select>
                     {
                         Array.from(filtre_existant).map(
-                            (filtre,i) => <option key={i} value={i}>{filtre.name}</option>
+                            (filtre_selected,i) => <option key={i} value={i} onClick={onFiltreChange}>{filtre_selected.name} { filtre_existant.map(function(e) { return e.function }).indexOf(filtre) === i ? designOrder[order] : null }</option>
                         )
                     }
                 </select>
             </div>
             <div className="product__content">
                 {
-                   render_image()
+                   render_image(filtre)
                 }
             </div>
         </section>
