460473f4b953fa700b7042c0d755254a4c2b6fbd
antony <antony.jacques06@gmail.com>
2021-10-20 22:59:54 +0200

    start lightbox

--- src/components/ContentProfile.js
+++ src/components/ContentProfile.js
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/tabindex-no-positive */
 import PropTypes from 'prop-types'
 import React, { useContext, useEffect, useState } from 'react'
 import { useUID } from 'react-uid'
 import { filtreImageContext } from '../context/filtreImageContext'
 import { imageLikedContext } from '../context/imageLiked'
 import '../css/contentprofile.scss'
+import '../css/lightbox.scss'
 import jsonData from '../data/site.json'
 import '../fontAwesome/css/all.min.css'
 import ListBox from './ListBox'
@@ -11,11 +14,56 @@ import ListBox from './ListBox'
 const ContentProfile = ({ photographer }) => {
   const { totalLikes, setTotalLikes } = useContext(imageLikedContext)
   const { filtreImageSelected } = useContext(filtreImageContext)
+  const [lightbox, setLightbox] = useState('')
+  const [indexlightbox, setIndexlightbox] = useState(null)
   const [medias, setMedias] = useState(
     jsonData.media
       .map((media) => (media.photographerId === photographer.id ? media : null))
       .filter((x) => x !== null)
   )
+
+  function checkTabPress(e) {
+    console.log(e)
+    // pick passed event or global event object if passed one is empty
+    let activeElement
+    if (e.keyCode === 9) {
+      // Here read the active selected link.
+      activeElement = document.activeElement
+      // If HTML element is an anchor <a>
+      if (
+        !(
+          activeElement.hasAttribute('tabIndex') &&
+          parseInt(activeElement.attributes.tabIndex.nodeValue, 10) >= 50 &&
+          parseInt(activeElement.attributes.tabIndex.nodeValue, 10) < 60
+        ) ||
+        activeElement.hasAttribute('lastbox')
+      ) {
+        console.log(
+          document.querySelectorAll(
+            '.product__content__item--show .fa-chevron-right'
+          )
+        )
+        document
+          .querySelectorAll(
+            '.product__content__item--show .fa-chevron-right'
+          )[0]
+          .focus()
+        e.stopPropagation()
+        e.preventDefault()
+      } else if (activeElement.hasAttribute('firstbox') && e.shiftKey) {
+        console.log(
+          document.querySelectorAll(
+            '.product__content__item--show *[lastbox]'
+          )[0]
+        )
+        document
+          .querySelectorAll('.product__content__item--show *[lastbox]')[0]
+          .focus()
+        e.stopPropagation()
+        e.preventDefault()
+      }
+    }
+  }
 
   useEffect(() => {
     let ret = 0
@@ -88,8 +136,72 @@ const ContentProfile = ({ photographer }) => {
     if (arrayRender !== medias) setMedias(arrayRender)
   }
 
-  const renderImage = (media) => (
-    <div className='product__content__item__image'>
+  const openLightBox = (key) => {
+    if (lightbox === '') {
+      setIndexlightbox(key)
+      setLightbox('lightbox')
+
+      document.querySelector('body').addEventListener('keydown', checkTabPress)
+    }
+  }
+
+  const changeIndexLightBox = (number) => {
+    console.log(number)
+    document
+      .getElementsByClassName('product__content__item--show')[0]
+      .classList.toggle('product__content__item')
+
+    document
+      .getElementsByClassName('product__content__item--show')[0]
+      .classList.toggle('product__content__item--show')
+
+    setIndexlightbox(indexlightbox + number)
+  }
+
+  useEffect(() => {
+    console.log(indexlightbox)
+    if (indexlightbox !== null) {
+      document
+        .getElementsByClassName('product__content__item')
+        [indexlightbox].classList.toggle('product__content__item--show')
+      document
+        .getElementsByClassName('product__content__item')
+        [indexlightbox].classList.toggle('product__content__item')
+
+      document
+        .querySelector(
+          '.product__content__item--show .product__content__item__image .fa-chevron-right'
+        )
+        .focus()
+    }
+  }, [indexlightbox])
+
+  const renderImage = (media, key) => (
+    <div
+      role='button'
+      className='product__content__item__image'
+      tabIndex={lightbox === '' ? 0 : -1}
+      onKeyDown={() => openLightBox(key)}
+      onClick={() => openLightBox(key)}>
+      {lightbox !== '' ? (
+        <span
+          firstbox='true'
+          role='button'
+          tabIndex={50}
+          onClick={() => changeIndexLightBox(-1)}
+          onKeyDown={(e) =>
+            verificationEvent(e)
+              ? () => {
+                  changeIndexLightBox(-1)
+                }
+              : console.log('bug')
+          }
+          type='button'
+          className='fas fa-chevron-left'
+        />
+      ) : (
+        ''
+      )}
       {media.image ? (
         <img alt={media.alt} src={`../img/${photographer.id}/${media.image}`} />
       ) : (
@@ -100,6 +212,25 @@ const ContentProfile = ({ photographer }) => {
           />
         </video>
       )}
+      {lightbox !== '' ? (
+        <span
+          lasttbox='true'
+          role='button'
+          tabIndex={51}
+          onClick={() => changeIndexLightBox(1)}
+          onKeyDown={(e) =>
+            verificationEvent(e)
+              ? () => {
+                  changeIndexLightBox(1)
+                }
+              : ''
+          }
+          type='button'
+          className='fas fa-chevron-right'
+        />
+      ) : (
+        ''
+      )}
     </div>
   )
 
@@ -107,7 +238,7 @@ const ContentProfile = ({ photographer }) => {
     applyFilter()
     return medias.map((media, i) => (
       <div key={useUID(media, i)} className='product__content__item'>
-        {renderImage(media)}
+        {renderImage(media, i)}
         <div className='product__content__item__description'>
           <div className='product__content__item__description__title'>
             <h2>{media.title}</h2>
@@ -127,11 +258,7 @@ const ContentProfile = ({ photographer }) => {
             }}
             onClick={(e) => toggleArrayItem(e, i)}>
             <p className='nbLike'>{media.likes}</p>
-            <button
-              type='button'
-              aria-label='likes'
-              className='fas fa-heart icon'
-            />
+            <span aria-label='likes' className='fas fa-heart icon' />
           </div>
         </div>
       </div>
@@ -139,7 +266,7 @@ const ContentProfile = ({ photographer }) => {
   }
 
   return (
-    <section className='product'>
+    <section className={`product ${lightbox}`}>
       <div className='product__filter'>
         <p id='order_label'>Trier par</p>
         <ListBox />
